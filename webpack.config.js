9fb707a88753e94dec26541dd85d8911b83c4caf
qiaopeng <a1067.qiao@neusoft.co.jp>
2018-01-20 19:44:08 +0900

    build success by webpack3 babel-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        path: 'dist',
-        filename: 'jsonschemaeditor.js',
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: "babel",
-            query: {
-                presets: ['es2015', 'react']
-            }
-        }, ],
-    }
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'jsonschemaeditorbootstrap.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader"
+      }
+    ]
+  }
 };
